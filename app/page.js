8011bd22d2711a5044a876f9cf02245ec19c33de
Vortex127 <mirza.business15@gmail.com>
2024-08-04 23:41:11 +0500

    Final

--- app/page.js
+++ app/page.js
@@ -180,11 +180,6 @@ export default function Home() {
 
   return (
     <main className="container max-w-2xl px-6 mx-auto">
-      <section className="py-3">
-        <small className="text-gray-500 text-md">My Balance</small>
-        <h2 className="text-4xl font-bold">{currencyformat(100000)}</h2>
-      </section>
-
       <section className="flex items-center gap-2 py-3">
         <button
           className="hover btn exp-inc-btns"
@@ -196,7 +191,7 @@ export default function Home() {
 
       {/* Expenses */}
       <section className="py-6">
-        <h3 className="text-2xl">My Expenses</h3>
+        <h3 className="text-2xl">My Pantry</h3>
         <div className="flex flex-col gap-4 mt-6">
           {items.map((item) => (
             <Pantry
